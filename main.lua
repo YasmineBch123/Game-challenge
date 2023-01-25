87644f83630f376e3fafb0a799eeb6bd63f0a6df
8moses <506869@vistacollege.nl>
2023-01-25 09:26:03 +0100

    f
    f

---
+++ main.lua
@@ -0,0 +1,44 @@
+--[[
+    Solitaire
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+require 'src/Dependencies'
+
+math.randomseed(os.time())
+
+local gameBoard = GameBoard()
+
+function love.load()
+    love.window.setTitle('Solitaire')
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    love.mouse.buttonsPressed = {}
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.mousepressed(x, y, button)
+    love.mouse.buttonsPressed[button] = true
+end
+
+function love.mouse.wasButtonPressed(button)
+    return love.mouse.buttonsPressed[button]
+end
+
+-- Triggered elke frame
+function love.update(dt)
+    gameBoard:update(dt)
+    
+    love.mouse.buttonsPressed = {}
+end
+
+function love.draw()
+    gameBoard:render()
+end
---
+++ src/Deck.lua
@@ -0,0 +1,48 @@
+--[[
+    Deck Class
+]]
+
+Deck = Class{}
+
+function Deck:init()
+    self.cards = {}
+
+    for i = 1, CARDS_IN_SUIT do
+        table.insert(self.cards, Card(i, HEARTS))
+        table.insert(self.cards, Card(i, DIAMONDS))
+        table.insert(self.cards, Card(i, CLUBS))
+        table.insert(self.cards, Card(i, SPADES))
+    end
+
+    self:shuffle()
+end
+
+-- return a card from the deck and remove it
+function Deck:draw()
+    local cardIndex = math.random(#self.cards)
+    local cardFromDeck = self.cards[cardIndex]
+    local cardToReturn = Card(cardFromDeck.face, cardFromDeck.suit)
+
+    table.remove(self.cards, cardIndex)
+
+    return cardToReturn
+end
+
+function Deck:shuffle()
+    local newCards = {}
+
+    for i = CARDS_IN_DECK, 1, -1 do
+        local randomIndex = math.random(#self.cards)
+        local card = self.cards[randomIndex]
+        local newCard = Card(card.face, card.suit, 0, 0)
+
+        table.insert(newCards, newCard)
+        table.remove(self.cards, randomIndex)
+    end
+
+    self.cards = newCards
+end
+
+function Deck:render()
+    -- pass
+end
